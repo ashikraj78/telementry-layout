6eba7b33d322c3205b854d83dbc890e5e858a102
ashikraj78 <ashikraj.78@gmail.com>
2020-05-18 17:28:12 +0530

    add changes in app

--- assetes/java-script/main.js
+++ assetes/java-script/main.js
@@ -1,3 +1,19 @@
+// top-header styling 
+let header = document.querySelector('.header');
+function styleHeader(){ 
+    let scrollPosition = Math.round(window.scrollY);
+    if(scrollPosition > 480){
+        header.classList.add("top-header");
+    }else{
+        header.classList.remove("top-header");
+    }
+}
+window.addEventListener("scroll",styleHeader);
+
+
+
+// parallax effect
+
 let parallax = document.getElementById("parallax");
 window.addEventListener("scroll", function(){
     let offset = window.scrollY;
@@ -5,46 +21,47 @@ window.addEventListener("scroll", function(){
 })
 
 
+
+//  about button styling
+
 let btn1 = document.querySelector('.bot1');
 let btn2 = document.querySelector('.bot2');
 let btn3 = document.querySelector('.bot3');
 let aliquam = document.querySelector(".aliquam");
 let consequat = document.querySelector(".consequat");
 let feugiat = document.querySelector(".feugiat");
-
+let active = "btn1"
 
 function showone(event){
-    btn1.style.transform ="scale(1.2)";
-    btn2.style.transform ="scale(1)"; 
-    btn3.style.transform ="scale(1)";
-    
     aliquam.style.display = "block";
     consequat.style.display="none";
     feugiat.style.display="none";
+    btn2.classList.remove('act')
+    btn3.classList.remove('act')
 }
 
 function showtwo(event){
-    btn1.style.transform ="scale(1)";
-    btn2.style.transform ="scale(1.2)"; 
-    btn2.style.zIndex="6"
-    btn3.style.transform ="scale(1)";  
-
     aliquam.style.display = "none";
     consequat.style.display="block";
     feugiat.style.display="none";
+    active = "btn2"
+    btn2.classList.add(active === "btn2" ? 'act' : '')
+    btn1.classList.remove('act')
+    btn3.classList.remove('act')
 }
 
 function showthree(event){
-    btn1.style.transform ="scale(1)";
-    btn2.style.transform ="scale(1)"; 
-    btn3.style.transform ="scale(1.2)";  
-
     aliquam.style.display = "none";
     consequat.style.display="none";
     feugiat.style.display="block";
+    active = "btn3"
+    btn3.classList.add(active === "btn3" ? 'act' : '')
+    btn1.classList.remove('act')
+    btn2.classList.remove('act')
 }
 
 
+btn1.classList.add(active === "btn1" ? 'act' : '')
 
 btn1.addEventListener("click",showone);
 btn2.addEventListener("click",showtwo);
